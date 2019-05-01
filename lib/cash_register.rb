6b04cb97839f05e96095b8f25539e5e85cbb593e
Anthony Rurup <tcrurup@gmail.com>
2019-05-01 02:37:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,8 +1,9 @@
 class CashRegister
   
-  attr_accessor :total
+  attr_accessor :total, :discount
   
   def initialize(employee_discount = 0)
     @total = 0
+    @discount = employee_discount
   end
 end
